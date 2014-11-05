bbf6a52a05e8cf9424a118977d801f3cd0d3b2ac
a3587556 <315803679@qq.com>
2014-11-05 16:32:04 +0800

    first commit

---
+++ FingerPaint/CanvasView.swift
@@ -0,0 +1,21 @@
+//
+//  CanvasView.swift
+//  FingerPaint
+//
+//  Created by darkgod on 14/10/31.
+//  Copyright (c) 2014年 darkgod. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FingerPaint/Path.swift
@@ -0,0 +1,21 @@
+//
+//  Path.swift
+//  FingerPaint
+//
+//  Created by darkgod on 14/11/5.
+//  Copyright (c) 2014年 darkgod. All rights reserved.
+//
+
+import UIKit
+
+class Path: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
